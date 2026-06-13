e31d38fdb397235982c1f6c1ea9c40c8c392c151
TOWUK <towuk@internet.ru>
2026-06-13 14:59:31 +0300

    Update BoostPlayers.java

--- src/BoostPlayers.java
+++ src/BoostPlayers.java
@@ -5,6 +5,6 @@ import mindustry.mod.Plugin;
 public class BoostPlayers extends Plugin {
     @Override
     public void init() {
-        Timer.schedule(() -> Groups.player.each(p -> { var u = p.unit(); if (u != null) { u.apply(StatusEffects.overdrive, 2f); } }), 0f, 1f);
+        Timer.schedule(() -> Groups.player.each(p -> { var u = p.unit(); if (u != null) { u.apply(StatusEffects.overdrive, 1f); } }), 0f, 1f);
     }
 }
